0052ab5bd53bfe39d6e4c2e366e73bd5f105305c
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-05-23 02:05:35 +0530

    example of reusing same html components using inheritance

---
+++ Web Development/Flask/Templates_example/template_inheritance.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('inheritance.html')
+
+
+@app.route('/puppy/<name>')
+def pup_name(name):
+    return render_template('puppy.html', name=name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
