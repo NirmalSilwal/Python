26f44e6f471bf0f91a80fecff8ba3a191af7f2a4
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-03-04 23:20:17 +0545

    file for establishing connection pipeline with database - mongod

---
+++ web scraping/flipcartCrawling/flipcartCrawling/pipelines.py
@@ -0,0 +1,34 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+import pymongo
+
+
+class FlipcartcrawlingPipeline:
+
+    def __init__(self):
+        # establishing connection with mongodb
+        #using local host
+        self.conn = pymongo.MongoClient('localhost', 27017)
+
+        # using mongo atlas, in cloud
+        # self.conn = pymongo.MongoClient('mongodb+srv://Nirmal:root@test1nirmal.tksus.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+        # creating database
+        db = self.conn['remoteDbNirmal']
+
+        # creating collection
+        self.collection = db['flipcart']
+
+    def process_item(self, item, spider):
+
+        # storing item in database collection
+        self.collection.insert(dict(item))
+
+        return item
